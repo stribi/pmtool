e43ac7a4554eff2ef391c4e3fc8da5adbd06b976
goran <goranpopovic@outlook.com>
2020-11-17 17:29:36 +0100

    Add Project Entity and H2 settings

---
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/PmtoolServerApplication.java
@@ -0,0 +1,13 @@
+package hr.from.goranpopovic.pmtoolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmtoolServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmtoolServerApplication.class, args);
+	}
+
+}
---
+++ pmtool-server/src/test/java/hr/from/goranpopovic/pmtoolserver/PmtoolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.from.goranpopovic.pmtoolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtoolServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
