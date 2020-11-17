56f52e7d312c650d683a49d9a724d52d2dc49458
goran <goranpopovic@outlook.com>
2020-11-17 18:14:11 +0100

    Add validation to Project

--- pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/controllers/ProjectController.java
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/controllers/ProjectController.java
@@ -1,8 +1,11 @@
 package hr.from.goranpopovic.pmtoolserver.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,10 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+		}
 		Project projectToSave = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(projectToSave, HttpStatus.CREATED);
 	}
