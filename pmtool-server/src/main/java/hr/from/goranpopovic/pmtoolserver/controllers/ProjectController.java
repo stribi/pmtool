e446b4e0e3ea66e90772b637840830241adb2d07
goran <goranpopovic@outlook.com>
2020-11-18 14:28:51 +0100

    Add deleteProject

--- pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/controllers/ProjectController.java
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/controllers/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,17 @@ public class ProjectController {
 	public ResponseEntity<?> getProjectById(@PathVariable String projectId){
 		Project project = projectService.findProjectByIdentifier(projectId.toUpperCase());
 		return new ResponseEntity<Project>(project, HttpStatus.OK);
+	}
+	
+	@GetMapping("/all")
+	public Iterable<Project> getAllProjects(){
+		return projectService.findAllProjects();
+	}
+	
+	@DeleteMapping("/{projectId}")
+	public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+		projectService.deleteProjectByIdentifier(projectId);
+		return new ResponseEntity<String>("Project with ID '" + projectId + "' was deleted", HttpStatus.OK);
 	}
 
 }
--- pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/services/ProjectService.java
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/services/ProjectService.java
@@ -32,6 +32,16 @@ public class ProjectService {
 		return project;
 	}
 	
+	public Iterable<Project> findAllProjects(){
+		return projectRepository.findAll();
+	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId);
+		if(project == null) throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. Project does not exists"); 
+		
+		projectRepository.delete(project);
+	}
 	
 
 }
