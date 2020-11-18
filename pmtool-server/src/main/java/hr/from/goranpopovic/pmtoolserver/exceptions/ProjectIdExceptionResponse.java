29ec06fce743e227ae4e3b3dcf483f27a00ab968
goran <goranpopovic@outlook.com>
2020-11-18 09:10:04 +0100

    Add Custom Exception for projectIdentifier

---
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,19 @@
+package hr.from.goranpopovic.pmtoolserver.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+	private String projectIdentifier;
+
+	public ProjectIdExceptionResponse(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+}
--- pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/services/ProjectService.java
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import hr.from.goranpopovic.pmtoolserver.domain.Project;
+import hr.from.goranpopovic.pmtoolserver.exceptions.ProjectIdException;
 import hr.from.goranpopovic.pmtoolserver.repositories.ProjectRepository;
 
 @Service
@@ -13,7 +14,15 @@ public class ProjectService {
 	private ProjectRepository projectRepository;
 	
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+		
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+			
+		} catch (Exception e) {
+			throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+		}
+		
 	}
 
 }
