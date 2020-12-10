c2f487ff4c5d318d5ca6b5a1ef7f490ee62f29b6
goran <goranpopovic@outlook.com>
2020-12-10 13:12:55 +0100

    Add OneToMany relationship Backlog ProjectTask

--- pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/domain/Backlog.java
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/domain/Backlog.java
@@ -1,11 +1,16 @@
 package hr.from.goranpopovic.pmtoolserver.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -25,6 +30,8 @@ public class Backlog {
 	private Project project;
 	
 	//OneToMany projecttask
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+	private List<ProjectTask> projectTasks = new ArrayList<ProjectTask>();
 	
 	public Backlog() {}
 
@@ -58,6 +65,14 @@ public class Backlog {
 
 	public void setProject(Project project) {
 		this.project = project;
+	}
+
+	public List<ProjectTask> getProjectTasks() {
+		return projectTasks;
+	}
+
+	public void setProjectTasks(List<ProjectTask> projectTasks) {
+		this.projectTasks = projectTasks;
 	}
 	
 	
---
+++ pmtool-server/src/main/java/hr/from/goranpopovic/pmtoolserver/services/ProjectTaskService.java
@@ -0,0 +1,5 @@
+package hr.from.goranpopovic.pmtoolserver.services;
+
+public class ProjectTaskService {
+
+}
